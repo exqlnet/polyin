29b9cf4e5afd19e2083eb6182a979c77387731b7
Exqlnet <exqlnet@gmail.com>
2020-02-22 21:30:12 +0800

    :bug: 修复一些bug

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+import decimal
 
 
 class Point:
@@ -22,14 +24,15 @@ class Line:
         self.b = -p1.x*(p2.y-p1.y) / (p2.x-p1.x) + p1.y if p1.x != p2.x else None
 
     def intersection(self, line2):
-        print("求交点: {0} + {1}".format(self, line2))
 
-        # 两直线水平或垂直
-        if (self.is_horizontal() and line2.is_horizontal()) or (self.is_vertical() and line2.is_vertical()):
+        # 两直线平行
+        if self.is_vertical() and line2.is_vertical():
             return None
 
         if self.k == line2.k:
             return None
+
+        # 相交
 
         # 有其中一条垂直
         if self.is_vertical():
@@ -44,7 +47,9 @@ class Line:
             y = round(self.k*x + self.b, 2)
 
         # 点在线段上：x在线段的范围内
-        if not (self.p1.x <= x <= self.p2.x and line2.p1.x <= x <= line2.p2.x):
+        r1 = (self.p2.x, self.p1.x) if self.p1.x >= self.p2.x else (self.p1.x, self.p2.x)
+        r2 = (line2.p2.x, line2.p1.x) if line2.p1.x >= line2.p2.x else (line2.p1.x, line2.p2.x)
+        if not (r1[0] <= x <= r1[1] and r2[0] <= x <= r2[1]):
             return None
 
         return Point(x, y)
@@ -72,7 +77,6 @@ class Polygen:
         self.points = points
         self.lines = []
         for i in range(-1, len(points) - 1):
-            print(i, i + 1)
             self.lines.append(Line(points[i], points[i+1]))
 
 
@@ -82,11 +86,6 @@ class Polygen:
 
         line2 = Line(point, point_b)
         line3 = Line(Point(2, -1), Point(2, 3))
-        print(line2)
-        print(line3)
-        print(line2.intersection(line3))
-
-        print(list(map(lambda l: l.intersection(line2), self.lines)))
 
         return len(list(filter(lambda p: p is not None, list(map(lambda l: l.intersection(line2), self.lines))))) % 2 == 1
 
@@ -97,16 +96,24 @@ class Polygen:
 
 if __name__ == "__main__":
 
-    p = Point(100, 300)
 
-    poly = Polygen(Point(-3, -2), Point(2, -1), Point(2, 3), Point(-100, 4))
-    print(poly.contains(Point(-50, 1)))
+    # 区域是南昌大学前湖校区，点是区域里的一点
+    poly = Polygen(Point(Decimal('28.6595744800'), Decimal('115.8178412500')), Point(Decimal('28.6704640100'), Decimal('115.8110525800')), 
+        Point(Decimal('28.6657077700'), Decimal('115.7966829800')), Point(Decimal('28.6427316600'),Decimal('115.7875711800')),
+        Point(Decimal('28.6396932200'), Decimal('115.7987466100')), Point(Decimal('28.6588107300'), Decimal('115.8179824100')))
+    print(poly.contains(Point(Decimal('28.6577095300'), Decimal('115.8007106600'))))
 
-    # l1 = Line(Point(-3, -2), Point(2, 3))
-    # l2 = Line(Point(-3, -1), Point(2, -1))
 
+    # p = Point(0.5, -0.5)
+    # points = [(-3, 0), (0, 4), (2, 0), (0, -2), (-2, -1)]
+
+    # poly = Polygen(*[Point(e[0], e[1]) for e in points])
+    # print(poly.contains(p))
+
+
+    # l1 = Line(Point(2, 0), Point(0, -2))
+    # l2 = Line(Point(0.5, -0.5), Point(10000, -0.5))
     # print(l1.intersection(l2))
-    # print(poly.contain(p))
 
 
 
